6e37c76600b46124c45072ce263906edbe1b1f50
ZinichZ <zaharomsk98@mail.ru>
2025-04-26 02:56:14 +0500

    #1_8_1
    fix: исправлена логика переключения классов day/night в компоненте Clock

--- src/1/1_8_1/App.tsx
+++ src/1/1_8_1/App.tsx
@@ -1,11 +1,5 @@
-// 1_8_1 Fix a broken clock
-/*
-  Этот компонент пытается установить CSS-класс <h1> на night в период с полуночи до шести часов утра, и на day во все остальное время. Однако это не срабатывает. Можете ли вы исправить этот компонент?
-
-  Вы можете проверить, работает ли ваше решение, временно изменив часовой пояс компьютера. Когда текущее время находится между полуночью и шестью часами утра, часы должны иметь инвертированные цвета!
-
-  Рендеринг — это вычисление, он не должен пытаться "делать" вещи. Можете ли вы выразить ту же идею по-другому?
-*/
+// #1_8_1
+// Исправлена логика переключения классов day/night в компоненте Clock
 
 export default function ClockWrapper() {
   return <Clock time={new Date()} />;
@@ -13,13 +7,7 @@ export default function ClockWrapper() {
 
 function Clock({ time }: { time: Date }) {
   const hours = time.getHours();
-  const elem = document.getElementById("time");
-  if (elem)
-    if (hours >= 0 && hours <= 6) {
-      elem.className = "night";
-    } else {
-      elem.className = "day";
-    }
-  return <h1 id="time">{time.toLocaleTimeString()}</h1>;
+  const className = hours >= 0 && hours < 6 ? "night" : "day";
+  return <h1 className={className}>{time.toLocaleTimeString()}</h1>;
 }
 
