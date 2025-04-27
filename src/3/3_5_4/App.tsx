82ebdc75be830b7ad6c48c22055eff9d53704c98
ZinichZ <zaharomsk98@mail.ru>
2025-04-28 03:00:25 +0500

    #3_5_4
    feat: реализован кастомный хук useReducer и заменён стандартный useReducer из React

--- src/3/3_5_4/App.tsx
+++ src/3/3_5_4/App.tsx
@@ -1,11 +1,8 @@
-// 3_5_4  Implement useReducer from scratch 
-/*
-  В предыдущих примерах вы импортировали хук useReducer из React. В этот раз вам предстоит реализовать хук useReducer самостоятельно! Вот заглушка для начала работы. Он не должен занимать более 10 строк кода.
+// #3_5_4
+// Реализован кастомный хук useReducer и заменён стандартный useReducer из React
 
-  Чтобы проверить свои изменения, попробуйте ввести текст в поле ввода или выбрать контакт.
-*/
-
-import { useReducer } from 'react';
+//import { useReducer } from 'react';
+import { useReducer } from './MyReact';
 import Chat from './Chat';
 import ContactList from './ContactList';
 import { initialState, messengerReducer } from './messengerReducer';
@@ -40,5 +37,5 @@ export type Contact = {
 const contacts = [
   {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
   {id: 1, name: 'Alice', email: 'alice@mail.com'},
-  {id: 2, name: 'Bob', email: 'bob@mail.com'},
+  {id: 2, name: 'Bob', email: 'bob@mail.com'}
 ];
