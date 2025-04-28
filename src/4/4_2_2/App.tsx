8edd62164c0cd3b678e00ee7350493d481873581
ZinichZ <zaharomsk98@mail.ru>
2025-04-28 17:55:23 +0500

    #4_2_2
    fix: установка фокуса на поле ввода при нажатии на кнопку

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -1,15 +1,20 @@
-// 4_2_2 Focus the search field
-/*
-  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
-*/
+// #4_2_2
+// Установка фокуса на поле ввода при нажатии на кнопку
+
+import { useRef } from 'react';
 
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={() => inputRef.current?.focus()}>
+          Search
+        </button>
       </nav>
       <input
+        ref={inputRef}
         placeholder="Looking for something?"
       />
     </>
